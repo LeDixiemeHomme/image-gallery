1a38605d587d59ab34f7b7433987c5e473f6af5c
Benoît Valle <benoit.valleea@hotmail.fr>
2021-02-09 13:41:56 +0100

    add access

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'beed201bb19c47169d4ffded0a5754eb,',
+    secretAccessKey: '138737db097ec1e0680fbcbcaf0bfa426f6cb3890efa9615',
+    endpoint: 's3.eu-de.cloud-object-storage.appdomain.cloud',
+    bucket: 'esgi-cloud-infra-files-9xzmd-bucket',
   },
   suffix: {
     small: '_small.jpg',
